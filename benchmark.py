9fc21ad5409376592823a1f76845975f14680fc6
Chris Mulligan <clm2186@columbia.edu>
2014-01-14 13:22:37 -0500

    Py: Holdout validation, print score.

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 #from kaggle user cclark
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import cross_validation
 from numpy import genfromtxt, savetxt
 
 def main():
@@ -9,10 +10,16 @@ def main():
     target = [x[0] for x in dataset]
     train = [x[1:] for x in dataset]
     test = genfromtxt(open('data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+
+    #build crossvalidation training set
+    train_train, train_test, target_train, target_test = cross_validation.train_test_split(train, target, test_size=0.4, random_state=0)
+    print train_train.shape
+    print train_test.shape
     
     #create and train the random forest
-    rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    rf.fit(train, target)
+    rf = RandomForestClassifier(n_estimators=50, n_jobs=2)
+    rf.fit(train_train, target_train)
+    print "crossval score is: ", rf.score(train_test, target_test)
 
     savetxt('data/submission.csv', rf.predict(test), delimiter=',', fmt='%f')
 
