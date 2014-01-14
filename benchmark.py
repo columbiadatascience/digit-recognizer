db54b791c0b842cce0c1f49f4c09dfa4b262b948
Chris Mulligan <clm2186@columbia.edu>
2014-01-14 12:49:34 -0500

    benchmark.py

---
+++ benchmark.py
@@ -0,0 +1,20 @@
+#from kaggle user cclark
+
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+
+def main():
+    #create the training & test sets, skipping the header row with [1:]
+    dataset = genfromtxt(open('data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
+    target = [x[0] for x in dataset]
+    train = [x[1:] for x in dataset]
+    test = genfromtxt(open('data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+    
+    #create and train the random forest
+    rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    rf.fit(train, target)
+
+    savetxt('data/submission.csv', rf.predict(test), delimiter=',', fmt='%f')
+
+if __name__=="__main__":
+    main()
